e60e5937e754462cb1c7fa505bdc6deac4930fd3
Gaurav72100 <gk240662@gmail.com>
2024-12-08 21:25:12 +0530

    Initial commit

---
+++ src/main/java/com/main/Main.java
@@ -0,0 +1,19 @@
+package com.main;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.model.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee e = ioc.getBean(Employee.class,"emps");
+		System.out.println(e);
+
+	}
+
+}
